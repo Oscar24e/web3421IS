368dcd06a18ed1b26638a13966c4d3c6cc8efa75
Oscar24e <benitez_1320114002@uptecamac.edu.mx>
2021-10-05 15:10:33 -0500

    hola

---
+++ PW DEBUG/index2.js
@@ -0,0 +1,22 @@
+var Datos = {
+    "nombre" : "Alejandra",
+    "apellido" : "Beltran",
+    "edad" : 20,
+    "escolaridad":{
+        "kinder" : "Mextli",
+        "primaria" : "Francisco gonzales boca negra",
+        "secundaria" : "Ma. del carmen molina rivero",
+        "preparatoria" : "Cbt no 1",
+        "universidad" : "UPTecamac"
+    },
+    "musica":{
+        "cancion1": "Daimond",
+        "cancion2": "Start boy",
+        "cancion3": "Cafune",
+        "Genero":{
+            "Reggae":"Rock",
+            "Pop":""
+        }
+
+    }
+}
---
+++ PW DEBUG/main.js
@@ -0,0 +1,56 @@
+// var n1 = 30;
+// var n2 = 20;
+// var test = 0;
+// if(n1 < n2) {
+//     console.log("n1 es menor n2");
+// }else{
+//     console.log("n1 mayor n2");
+//     test = suma();
+// }
+// n1 = 100;
+
+// console.log(n1);
+// console.log(test);
+
+// function suma(){    
+//     let resultado = n1 + n2;
+//     return resultado;
+// }
+
+// var elementos = [];
+// var elementos2 = Array();
+// var elementos3 = ["hola",1,8.5,true,5,7,8,9,10];
+// var elementos4 = [];
+// var palabra = "Hola mundo"
+// var contenedor = [];
+// var ejemplo = ["H","o","l","a","m","u","n","d","o"];
+
+
+// for(let i = 0; i < elementos3.length; i++){
+//     console.log(elementos3[i]);
+//     elementos4.push(elementos3[i]);
+// }
+
+// console.log(elementos4);
+var Datos = {
+    "nombre" : "Oscar",
+    "apellido" : "Benitez",
+    "edad" : 20,
+    "escolaridad":{
+        "kinder" : "Benjamin",
+        "primaria" : "Justo sierra",
+        "secundaria" : "Sor juana ines de la cruz",
+        "preparatoria" : "Epoem 14",
+        "universidad" : "UPTecamac"
+    },
+    "musica":{
+        "cancion1": "muerte en hawaii",
+        "cancion2": "Gimme the power",
+        "cancion3": "te ire a buscar",
+        "Genero":{
+            "Rap":"Regae",
+            "Banda": "Electronica"
+        }
+
+    }
+}
